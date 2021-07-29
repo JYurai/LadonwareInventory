8f70098a63f63d6b10fe65c4e36a50927110588a
JYurai <87451941+JYurai@users.noreply.github.com>
2021-07-29 14:02:34 -0500

    Add files via upload

---
+++ ui/src/app/products/add-edit-products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ShowProductsComponent } from './show-products/show-products.component';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
